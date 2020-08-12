2fcc1b7057e5a69620610f5938bb9886df267ab5
Sagarika Bathala <sagarika.bathala@gmail.com>
2020-08-12 16:52:53 +0200

    init project

---
+++ src/test/java/com/mobiquity/userblogapiitest/UserBlogApiItestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobiquity.userblogapiitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserBlogApiItestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
