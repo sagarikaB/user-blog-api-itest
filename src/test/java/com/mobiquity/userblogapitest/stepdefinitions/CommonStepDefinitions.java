a6c5e0451f7025309d246b4eb43185114877c21d
balakrishna gondesi <sagarika.bathala@gmail.com>
2020-08-15 21:30:37 +0200

    move application url to properties

--- src/test/java/com/mobiquity/userblogapitest/stepdefinitions/CommonStepDefinitions.java
+++ src/test/java/com/mobiquity/userblogapitest/stepdefinitions/CommonStepDefinitions.java
@@ -2,11 +2,14 @@ package com.mobiquity.userblogapitest.stepdefinitions;
 
 import io.cucumber.java.Before;
 import io.restassured.RestAssured;
+import org.springframework.beans.factory.annotation.Value;
 
 public class CommonStepDefinitions {
+    @Value("${application.url}")
+    private String applicationUrl;
     @Before
     public void setup() {
-        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+        RestAssured.baseURI = applicationUrl;
         RestAssured.port = 443;
     }
 }
